9bc435f36cd060e40fb7268597933c305123bc1d
Chris <none>
2021-11-20 13:14:36 -0800

    Update react-scripts dependency

--- database/db.js
+++ database/db.js
@@ -45,7 +45,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req, res, next) => {
     res.header(
       "Access-Control-Allow-Origin",
-      "https://radiant-crag-56357.herokuapp.com"
+      "https://localhost:3000"
     );
     res.header(
       "Access-Control-Allow-Headers",
